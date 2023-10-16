a85d41a7dbcbc8fc21b4296a76a715964ceb9dd9
Agedanna <annabeatryz12345@gmail.com>
2023-10-15 23:27:06 -0300

    port

---
+++ app/src/components/Footer/Footer.js
@@ -0,0 +1,14 @@
+import './Footer.css'
+
+const Footer = () => (
+  <footer className='footer'>
+    <a
+      href='https://github.com/AgedAnna'
+      className='link footer__link'
+    >
+      Created By Anna Beatryz
+    </a>
+  </footer>
+)
+
+export default Footer
